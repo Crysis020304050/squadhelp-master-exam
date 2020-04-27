58740cd775e970f60e06f4415d326a05dcf7bd3c
Crysis020304050 <crysiswarhead020304050@gmail.com>
2020-04-27 14:07:39 +0300

    Add events sorting to events reducer

--- client/src/reducers/eventsReducer.js
+++ client/src/reducers/eventsReducer.js
@@ -1,4 +1,5 @@
 import ACTION from '../actions/actionTypes';
+import _ from 'lodash';
 
 const initialState = {
     events: [],
@@ -7,8 +8,20 @@ const initialState = {
 export default function (state = initialState, action) {
     switch (action.type) {
         case ACTION.CREATE_NEW_EVENT: {
+            const sortedEvents = _.clone(state.events);
+            sortedEvents.push(action.event);
+            sortedEvents.sort(((a, b) => (a.endTime - b.endTime)));
+            sortedEvents.sort((a, b) => {
+                if (a.endTime - new Date() < 0 && b.endTime - new Date() > 0) {
+                    return 1;
+                }
+                else if (a.endTime - new Date() > 0 && b.endTime - new Date() < 0) {
+                    return -1;
+                }
+                return 0;
+            });
             return {
-                events: [...state.events, action.event]
+                events: sortedEvents,
             }
         }
         default:
